e1729e12ab193715ae0ce70d00e1142d534b3998
Gary Ewan Park <gep13@gep13.co.uk>
2019-04-05 10:22:49 +0100

    (GH-7) Initial attempt at getting LSP Client running

---
+++ src/chocolatey-vs/ChocolateyLanguageClient.cs
@@ -0,0 +1,68 @@
+﻿
+
+using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Threading;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace chocolatey_vs
+{
+    [ContentType("nuspec")]
+    [Export(typeof(ILanguageClient))]
+    public class ChocolateyLanguageClient : ILanguageClient
+    {
+        public string Name => "Chocolatey Language Server Client";
+
+        public IEnumerable<string> ConfigurationSections => null;
+
+        public object InitializationOptions => null;
+
+        public IEnumerable<string> FilesToWatch => null;
+
+        public event AsyncEventHandler<EventArgs> StartAsync;
+        public event AsyncEventHandler<EventArgs> StopAsync;
+
+        public async Task<Connection> ActivateAsync(CancellationToken token)
+        {
+            await Task.Yield();
+
+            var info = new ProcessStartInfo();
+            info.FileName = "dotnet";
+            info.Arguments = "C:\\github_local\\gep13\\chocolatey-vs\\lib\\ChocolateyLanguageServer\\Chocolatey.Language.Server.dll";
+            info.RedirectStandardInput = true;
+            info.RedirectStandardOutput = true;
+            info.UseShellExecute = false;
+            info.CreateNoWindow = true;
+
+            Process process = new Process();
+            process.StartInfo = info;
+
+            if (process.Start())
+            {
+                return new Connection(process.StandardOutput.BaseStream, process.StandardInput.BaseStream);
+            }
+
+            return null;
+        }
+
+        public async Task OnLoadedAsync()
+        {
+            await StartAsync?.InvokeAsync(this, EventArgs.Empty);
+        }
+
+        public Task OnServerInitializedAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnServerInitializeFailedAsync(Exception e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/chocolatey-vs/NuspecContentDefinition.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace chocolatey_vs
+{
+    public class NuspecContentDefinition
+    {
+        [Export]
+        [Name("nuspec")]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition NuspecContentTypeDefinition;
+
+
+        [Export]
+        [FileExtension(".nuspec")]
+        [ContentType("nuspec")]
+        internal static FileExtensionToContentTypeDefinition NuspecFileExtensionDefinition;
+    }
+}
